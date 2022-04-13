5ac8f6d3e5e5365162d2948cb475865b628b02bd
padxing <padxing@123.com>
2022-04-13 16:25:04 +0800

    master test1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hello git333!!");
         System.out.println("hello git444!!");
         System.out.println("hot-fix change 1");
-        System.out.println("master change 1");
+        System.out.println("master change 11111");
     }
 }
