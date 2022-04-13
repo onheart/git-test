861ef77176089162ff29e9a73da2c7169e78fdd3
padxing <padxing@123.com>
2022-04-13 16:13:14 +0800

    hot-fix commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git!!");
         System.out.println("hello git!!");
         System.out.println("hello git333!!");
+        System.out.println("hello git444!!");
     }
 }
