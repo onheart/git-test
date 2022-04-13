9d311bcb5a3aeb5ce83f4345fd370e950ccf6c16
padxing <padxing@123.com>
2022-04-13 16:06:59 +0800

    second commit

--- src/main/java/com/atguigu/GitTest2.java
+++ src/main/java/com/atguigu/GitTest2.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hello gitTest2!!");
+        System.out.println("hello gitTest2!!");
     }
 }
